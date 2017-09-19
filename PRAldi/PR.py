b263fff1fe33494c4560ac2ea4f9cddaf42dcb06
tandiawanedgar <32050623+tandiawanedgar@users.noreply.github.com>
2017-09-19 22:59:59 +0700

    Add files via upload

---
+++ PRAldi/PR.py
@@ -0,0 +1,13 @@
+data=['a','b','c','d','e','f','g','h','i','j']
+a=0
+b=1
+c=int(a)+int(b)
+d=int(c)+int(b)
+e=int(d)+int(c)
+f=int(e)+int(d)
+g=int(f)+int(e)
+h=int(g)+int(f)
+i=int(h)+int(g)
+j=int(i)+int(h)
+print("fibonacci number")
+print(a,b,c,d,e,f,g,h,i,j)
